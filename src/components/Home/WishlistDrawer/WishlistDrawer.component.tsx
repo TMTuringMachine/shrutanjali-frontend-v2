813dea3db39933076efb89206409f43cc270457c
gokhalevedant06 <gokhalevedant06@gmail.com>
2023-12-29 14:01:28 +0530

    Fixed Wishlist Drawer View

--- src/components/Home/WishlistDrawer/WishlistDrawer.component.tsx
+++ src/components/Home/WishlistDrawer/WishlistDrawer.component.tsx
@@ -12,6 +12,8 @@ import { songs } from "../../../helpers/data";
 import SongPreview from "../SongPreview/SongPreview.component";
 import useWishlist from "../../../hooks/useWishlist";
 import useMedia from "../../../hooks/useMedia";
+import { Icon } from "@iconify/react";
+import {EmptyWishList} from '../../../pages/Explore/explore.styles'
 interface Props {
   state: boolean;
   toggleDrawer: Function;
@@ -19,17 +21,22 @@ interface Props {
 
 const WishlistDrawer: FunctionComponent<Props> = ({ state, toggleDrawer }) => {
   const { getWishlist } = useWishlist()
+  const [zero,setZero] = useState(false);
   const [wishlist, setWishlist] = useState([]);
   const { populateMedia } = useMedia();
   const populateWishlist = async () => {
-
     const list = getWishlist();
+    if(list.length===0){
+      setZero(true);
+      return;
+    }
     if (!list) return;
-    setWishlist(await populateMedia(list));
+    const data = await populateMedia(list)
+    if(data.length!==0) setZero(false)
+    setWishlist(data); 
   }
 
   useEffect(() => {
-    // setWishlist(getWishlist());
     populateWishlist()
   }, [state])
 
@@ -50,9 +57,20 @@ const WishlistDrawer: FunctionComponent<Props> = ({ state, toggleDrawer }) => {
                 <SongPreview song={song} />
               </Grid>
             ))}
-          </Grid></> : <>Your Wishlist Is Empty!</>
+          </Grid></> : <></>
         }
 
+        {zero && (
+            <EmptyWishList> 
+              <Icon icon="ri:heart-add-fill" width="50px" height="50px" />
+              <Typography sx={{ fontSize: "1.5em" }}>
+                Your wishlist is empty!
+              </Typography>
+              <Typography sx={{ color: "gray" }}>
+                Add songs to your wishlist to enjoy the songs special to you !
+              </Typography>
+            </EmptyWishList>
+        )}
 
       </WishlistDrawerContainer>
     </StyledWishlistDrawer>
