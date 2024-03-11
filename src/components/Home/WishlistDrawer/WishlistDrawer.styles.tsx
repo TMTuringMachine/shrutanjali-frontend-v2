8e89a84857fafd653029b5cadda1815b354f0df4
Aryan Shinde <aryan.shinde20@vit.edu>
2024-03-12 00:05:12 +0530

    added view port

--- src/components/Home/WishlistDrawer/WishlistDrawer.styles.tsx
+++ src/components/Home/WishlistDrawer/WishlistDrawer.styles.tsx
@@ -1,37 +1,38 @@
-import { Box, Drawer, styled } from "@mui/material";
+import { Box, Drawer, styled } from '@mui/material';
 
 export const WishlistDrawerContainer = styled(Box)(({ theme }) => ({
-  width: "100%",
-  height: "100%",
+  width: '100%',
+  height: '100%',
   backgroundColor: theme.palette.background.default,
   padding: theme.spacing(2),
-  display: "flex",
-  flexDirection: "column",
-  overflow: "auto",
-  gap: "25px",
-  alignItems: "center",
-  "& .modalHeader": {
-    fontSize: "1.5em",
+  display: 'flex',
+  flexDirection: 'column',
+  overflow: 'auto',
+  gap: '25px',
+  alignItems: 'center',
+  '& .modalHeader': {
+    fontSize: '1.5em',
     fontWeight: 900,
   },
 }));
 
-export const StyledWishlistDrawer = styled(Drawer)(({theme}) => ({
-  "& .MuiPaper-root": {
-    width: "50vw",
-    height: "fit-content",
-    maxHeight: "80vh",
-    overflow: "hidden",
-    margin: "50px 0px",
-    borderRadius: "20px",
-    left: "25vw",
+export const StyledWishlistDrawer = styled(Drawer)(({ theme }) => ({
+  '& .MuiPaper-root': {
+    width: '50vw',
+    height: 'fit-content',
+    maxHeight: '80vh',
+    overflow: 'hidden',
+    margin: '50px 0px',
+    borderRadius: '20px',
+    left: '25vw',
     // transform: "translate(-50%,0%)",
-    position: "absolute",
+    position: 'absolute',
   },
-  [theme.breakpoints.down('md')]:{
-    '& .MuiPaper-root':{
-      width:'90vw',
-      left:'5vw'
-    }
-  }
+  [theme.breakpoints.down('md')]: {
+    '& .MuiPaper-root': {
+      width: '90vw',
+      left: '5vw',
+      minHeight: '50vh',
+    },
+  },
 }));
